2ed6d6a05f78568ca809ab3aef2cb12d8e93e925
Skalmar <ana.rei.014@gmail.com>
2017-10-31 15:42:17 +0100

    zapis zapytań sql

---
+++ PracowaniaProgramowania/src/main/java/PROJECT_1/Main.java
@@ -0,0 +1,33 @@
+package PROJECT_1;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args)throws FileNotFoundException{
+        String numer_zadania;
+        String zapytanie_sql;
+
+        String yes_no="YES";
+        Scanner scanner = new Scanner(System.in);
+
+        Parsing Parse = new Parsing();
+        Add Adding = new Add();
+
+        while(!yes_no.equals("NO")) {
+            System.out.println("Podaj numer zadania");
+            numer_zadania = scanner.nextLine();
+            System.out.println("Podaj zapytanie SQL");
+            zapytanie_sql = scanner.nextLine();
+
+            if(Parse.check_sql(zapytanie_sql)){
+                Adding.Add(numer_zadania,zapytanie_sql);
+            }
+
+            System.out.println("Do you wish to continue? YES/NO ");
+            yes_no=scanner.nextLine();
+
+        }
+        Adding.execute();
+        Adding.close();
+    }
+}
