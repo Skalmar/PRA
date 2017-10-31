32dc9ebdc1bd033c0cce583b533aa180678e563a
Skalmar <ana.rei.014@gmail.com>
2017-10-31 15:16:55 +0100

    Update Parsing.java

--- PracowaniaProgramowania/src/main/java/PROJECT_1/Parsing.java
+++ PracowaniaProgramowania/src/main/java/PROJECT_1/Parsing.java
@@ -2,7 +2,7 @@ package PROJECT_1;
 
 public class Parsing {
     String sql;
-    public void check_sql(String clause) {
+    public boolean check_sql(String clause) {
         clause += ' ';
         int i;
         int l = clause.length();
@@ -11,25 +11,26 @@ public class Parsing {
         for (i = 0; i < l; i++) {
             if (clause.charAt(i) == ' ') {
                 System.out.println(clause.substring(x, i));
-                if ( clause.substring(x, i).equals("SELECT")) {
+                if (clause.substring(x, i).equals("SELECT")) {
                     sql += 1;
                 } else if (clause.substring(x, i).equals("FROM")) {
                     sql += 2;
-                } else if  (clause.substring(x, i).equals("WHERE")) {
+                } else if (clause.substring(x, i).equals("WHERE")) {
                     sql += 3;
-                } else if (clause.substring(x, i).equals("ORDER BY")) {
+                } else if (clause.substring(x, i).equals("ORDER")) {
                     sql += 4;
+                } else if (clause.substring(x, i).equals("BY")) {
+                    sql += 5;
                 }
                 x = i + 1;
             }
-            if((sql.equals("12")) || (sql.equals("123")) || (sql.equals("124")) ||(sql.equals("1234")))
-            {
-                System.out.println("Thumbs up!");
             }
-            else
-            {
+            if ((sql.equals("12")) || (sql.equals("123")) || (sql.equals("1245")) || (sql.equals("12345"))) {
+                System.out.println("Thumbs up!");
+                return true;
+            } else {
                 System.out.println("ERROR: wrong order of clauses in a sql query!");
+                return false;
             }
         }
     }
-}
